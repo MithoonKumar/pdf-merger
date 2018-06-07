a664542b0f10a8ec534c7aad7b30b30612d06e30
tikTokTik <mithoonk@flock.com>
2018-06-07 14:22:51 +0530

    First commit

---
+++ src/main/java/com/example/pdfmergerproject/PdfMergerProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.pdfmergerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfMergerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfMergerProjectApplication.class, args);
+	}
+}
